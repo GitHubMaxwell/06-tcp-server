be152973ad8fc2e4cabf667c7eac2becfc458a0a
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-06-04 22:03:49 -0700

    it works but fails on travis

--- index.js
+++ index.js
@@ -10,44 +10,92 @@ testing is not required.
 
 'use strict';
 //1st and 3rd party modules to require and 
-const EE = require('event'); //node module that gives access to 
+const EE = require('events'); //node module that gives access to 
 const eventEmit = new EE(); //
 const net = require('net'); //node module used for creating the server
 const uuid = require('uuid/v4'); //way to identify individual users
 const port = process.env.PORT || 3003;
 const server = net.createServer(); //creating a "server isntance"
-const socketPool = {};
+let socketPool = {};
 ////////////////////////////////////
 
 //user constructor
 let User = function(socket) {
   let id = uuid();
   this.id = id;
-  this.nick = `Nickname`;
+  this.nick = `Nickname ${id}`;
   this.socket = socket;
 };
 
 //connection listener / on user connection to the server
 server.on('connection', (socket) => {
   let user = new User(socket);
+  //called a dictionary
   socketPool[user.id] = user;
   console.log(`New User Added`);
-  //what is this data event and why does it 
-  socket.on('data', (buffer) => { disAct(user.id, buffer);
-  });
+  socket.on('data', (buffer) => disAct(user.id,buffer));
 });
 
-//dispatchAction = function that takes in the user.id and the buffer (that was derived from 'data' event) and PARSES the command buffer
+
 let disAct = (userId, buffer) => {
-  // the buffer comes in as an object so we have to convert to string
   let entry = parse(buffer);
+  entry && eventEmit.emit(entry.command, entry, userId);
 };
 
 //command parser (when entering commands into telnet)
+let parse = (buffer) => {
+  let text = buffer.toString().trim();
+
+  if(!text.startsWith('@')) { 
+    return console.log(`${text} is an incorrect command`);
+  }
+  let [command,payload] = text.split(/\s+(.*)/);
+  let [target,message] = payload ? payload.split(/\s+(.*)/) : [];
+  return {command,payload,target,message};
+};
 
 //command handlers
+eventEmit.on('@all', (data, userId) => {
+  for( let connection in socketPool ) {
+    console.log('connection: ', connection);
+    let user = socketPool[connection];
+    user.socket.write(`<${socketPool[userId].nick}>: ${data.payload}\n`);
+  }
+});
 
-//server listener
+eventEmit.on('@exit', (data, userId) => {
+  socketPool[userId].socket.destroy();
+  console.log(`${socketPool[userId].nick} left the game`);
+  delete socketPool[userId];
+});
+
+eventEmit.on('@nick', (data, userId) => {
+  socketPool[userId].nick = data.target;
+  console.log(`${socketPool[userId].id} changed their nickname to ${socketPool[userId].nick}`);
+});
+
+eventEmit.on('@dm', (data, userId) => {
+  let findUser = data.target;
+  for (let find in socketPool) {
+    if (findUser === socketPool[find].nick) {
+      let dmUser = socketPool[find];
+      dmUser.socket.write(`FROM ${socketPool[userId].nick} TO ${data.target}>::::: ${data.message}`);
+    }
+  }
+});
+
+eventEmit.on('@list', (data ,userId) => {
+  console.log('SOCKET POOL>>>>>',socketPool);
+  let userArr = [];
+  for ( let connection in socketPool ) {
+    let user = socketPool[connection];
+    userArr.push(user.nick);
+  }
+  console.log('USER ID>>>>', userId);
+  socketPool[userId].socket.write(`The List contains: ${userArr}`);
+});
+
+
 server.listen(port, ()=>{
   console.log('ITS ALIVE');
 });
---
+++ src/README.md
@@ -0,0 +1,5 @@
+##  Documentation  
+Write basic documention for starting your server connection and using the chatroom application.  Be sure to use proper markdown constructs and `highlight blocks of code`.
+* open two terminal windows both in the root directory of this lab
+* in one window you can run nodemon index.js and in the other "telnet (your IP) (PORT)
+* the @ commands work as described in the MVP 
